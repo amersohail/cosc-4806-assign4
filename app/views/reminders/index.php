cd61ca9ecbf52251e47099ee30e2db44515a23fc
amersohail <amersohail7@gmail.com>
2024-06-27 21:46:57 +0000

    created reminder controller, reminder model and reminder view.

---
+++ app/views/reminders/index.php
@@ -0,0 +1,15 @@
+<?php require_once 'app/views/templates/header.php' ?>
+
+<div class="container">
+    <div class="page-header" id="banner">
+        <h1>Reminders</h1>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-12">
+            <p> <a href="/logout">Click here to logout</a></p>
+        </div>
+    </div>
+
+    <?php require_once 'app/views/templates/footer.php' ?>
