108b816c626123d96ccd525405e34170a0bd6b11
amersohail <amersohail7@gmail.com>
2024-06-29 01:43:18 +0000

    renamed "reminders" to "notes"

--- app/models/Reminder.php
+++ app/models/Reminder.php
@@ -7,7 +7,7 @@ class Reminder {
 
     public function get_all_reminders () {
       $db = db_connect();
-      $statement = $db->prepare("SELECT * FROM reminders WHERE deleted IS NULL OR deleted = '' OR deleted = 0;");
+      $statement = $db->prepare("SELECT * FROM notes WHERE deleted IS NULL OR deleted = '' OR deleted = 0;");
       $statement->execute();
       $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
       return is_array($rows) ? $rows : [$rows];
@@ -15,7 +15,7 @@ class Reminder {
 
     public function get_reminder_by_id($id) {
         $db = db_connect();
-        $statement = $db->prepare("SELECT * FROM reminders WHERE id = :id AND deleted = 0");
+        $statement = $db->prepare("SELECT * FROM notes WHERE id = :id AND deleted = 0");
         $statement->bindParam(':id', $id, PDO::PARAM_INT);
         $statement->execute();
         return $statement->fetch(PDO::FETCH_ASSOC);
@@ -23,13 +23,13 @@ class Reminder {
     
     public function create_reminder($user_id, $subject) {
           $db = db_connect();
-          $query = $db->prepare("INSERT INTO reminders (user_id, subject) VALUES (?, ?)");
+          $query = $db->prepare("INSERT INTO notes (user_id, subject) VALUES (?, ?)");
           return $query->execute([$user_id, $subject]);
     }
     
     public function update_reminder($id, $subject, $completed) {
         $db = db_connect();
-        $statement = $db->prepare("UPDATE reminders SET subject = :subject, completed = :completed WHERE id = :id");
+        $statement = $db->prepare("UPDATE notes SET subject = :subject, completed = :completed WHERE id = :id");
         $statement->bindParam(':subject', $subject, PDO::PARAM_STR);
         $statement->bindParam(':completed', $completed, PDO::PARAM_INT);
         $statement->bindParam(':id', $id, PDO::PARAM_INT);
@@ -39,7 +39,7 @@ class Reminder {
     //not deleting the row, just setting the delete column value to true.
     public function delete_reminder($id) {
         $db = db_connect();
-        $statement = $db->prepare("UPDATE reminders SET deleted = 1 WHERE id = :id");
+        $statement = $db->prepare("UPDATE notes SET deleted = 1 WHERE id = :id");
         $statement->bindParam(':id', $id, PDO::PARAM_INT);
         $statement->execute();
     }
